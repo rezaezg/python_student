dd13806683832c27388154cc9067642119fd3680
rezaezg <98477617+rezaezg@users.noreply.github.com>
2023-07-04 13:33:09 +0200

    Create height_triangle.py
    add an example

---
+++ height_triangle.py
@@ -0,0 +1,14 @@
+""" numerical triangle of height N-1
+1
+22
+333
+4444
+55555 """
+
+# with range
+for i in range(1, int(input())):
+    print(i * (10 ** i // 9))
+
+# with range and lambda
+for n in range(1,int(input())):
+    print(sum(map(lambda i: n * 10**i, range(n))))
