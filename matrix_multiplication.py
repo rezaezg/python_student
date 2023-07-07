df05c103e8b44426842b88fa713b0df3e36f6ed1
rezaezg <98477617+rezaezg@users.noreply.github.com>
2023-07-07 10:00:56 +0200

    Create matrix_multiplication.py
    multiplication matrix added

---
+++ matrix_multiplication.py
@@ -0,0 +1,17 @@
+def matrix_multiplication(matrix1, matrix2):
+    rows1 = len(matrix1)
+    cols1 = len(matrix1[0])
+    rows2 = len(matrix2)
+    cols2 = len(matrix2[0])
+
+    if cols1 != rows2:
+        raise ValueError("The number of columns in matrix1 should be equal to the number of rows in matrix2.")
+
+    result = [[0] * cols2 for _ in range(rows1)]
+
+    for i in range(rows1):
+        for j in range(cols2):
+            for k in range(cols1):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+
+    return result
